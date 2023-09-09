71a2b6cb7f2b7f66f64eceeec5a58241139b8b98
Emiliano Aloi <emilianoaloi.dev@gmail.com>
2023-09-09 19:33:29 -0300

    se trabajo preferencia

--- server.js
+++ server.js
@@ -14,36 +14,142 @@ app.get('/', function (req, res) {
     res.send('el servidor de mercadopago funciona');
 })
 
-app.post("/create_preference", (req, res) => {
-    let preference = {
-        items: [
-            {
-                title: req.body.description,
-                unit_price: Number(req.body.price),
-                quantity: Number(req.body.quantity),
-            },
-        ],
-        back_urls: {
-            success: "http://localhost:3000",
-            failure: "http://localhost:3000",
-            pending: "",
-        },
-        auto_return: "approved",
-    };
+ 
 
-    mercadopago.preferences
-        .create(preference)
-        .then(function (response) {
-            res.json({
-                id: response.body.id,
-            });
-        })
-        .catch(function (error) {
-            console.log(error);
+app.post("/create_preference", async (req, res) => {
+    try {
+        const preference = {
+            items: [
+                {
+                    title: req.body.description,
+                    unit_price: Number(req.body.price),
+                    quantity: Number(req.body.quantity),
+                },
+            ],
+            back_urls: {
+                success: "http://localhost:3000",
+                failure: "http://localhost:3000",
+                pending: "",
+            },
+            auto_return: "approved",
+        };
+
+        const response = await mercadopago.preferences.create(preference);
+
+        res.json({
+            id: response.body.id,
         });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Error al crear la preferencia' });
+    }
 });
+
 
 
 app.listen(8080, () => {
     console.log('server is running on port 8080');
 });
+
+
+
+
+
+
+
+///////////////////////
+
+
+
+// app.post("/create_preference", async (req, res) => {
+//     try {
+//         const items = req.body.items; // Obtiene el array de productos del cuerpo de la solicitud
+
+//         // Crea un array de objetos de items para la preferencia de MercadoPago
+//         const preferenceItems = items.map(item => ({
+//             title: item.title,
+//             unit_price: Number(item.unit_price),
+//             quantity: Number(item.quantity),
+//             description: item.description,
+//             picture_url: item.picture_url, // Agrega la URL de la imagen del producto
+//             color: item.color, // Agrega el color del producto
+//             size: item.size, // Agrega el tamaño del producto
+//         }));
+
+//         // Construye la preferencia con los items actualizados
+//         let preference = {
+//             items: preferenceItems,
+//             back_urls: {
+//                 success: "http://localhost:3000",
+//                 failure: "http://localhost:3000",
+//                 pending: "",
+//             },
+//             auto_return: "approved",
+//         };
+
+//         const response = await mercadopago.preferences.create(preference);
+
+//         res.json({
+//             id: response.body.id,
+//         });
+//     } catch (error) {
+//         console.log(error);
+//         res.status(500).json({ error: 'Error al crear la preferencia' });
+//     }
+// });
+
+
+//////////////////////
+/////////////////
+
+
+// app.post("/create_preference", async (req, res) => {
+//     try {
+
+        
+//         const items = req.body.items; // Obtiene el array de productos del cuerpo de la solicitud
+
+
+//         const preferenceItems = items.map(item => ({
+//             title: item.name, // Usar la propiedad del objeto en el array
+//             unit_price: Number(item.newTotal), // Usar la propiedad del objeto en el array
+//             quantity: Number(item.quantity),
+//             picture_url: item.img, // Usar la propiedad del objeto en el array
+//             color: item.color, // Usar la propiedad del objeto en el array
+//             size: item.size, // Usar la propiedad del objeto en el array
+//         }));
+
+
+//         let preference = {
+
+//             items: preferenceItems,
+
+//             back_urls: {
+//                 success: "http://localhost:3000",
+//                 failure: "http://localhost:3000",
+//                 pending: "",
+//             },
+//             auto_return: "approved",
+//         };
+
+//         //     mercadopago.preferences
+//         //         .create(preference)
+//         //         .then(function (response) {
+//         //             res.json({
+//         //                 id: response.body.id,
+//         //             });
+//         //         })
+//         //         .catch(function (error) {
+//         //             console.log(error);
+//         //         });
+
+//         const response = await mercadopago.preferences.create(preference);
+
+//         res.json({
+//             id: response.body.id,
+//         });
+//     } catch (error) {
+//         console.log(error);
+//         res.status(500).json({ error: 'Error al crear la preferencia' });
+//     }
+// });
