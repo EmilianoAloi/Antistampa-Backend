18d6b8f6d9cbe42560893218a4af1d1b577e6369
Emiliano Aloi <emilianoaloi.dev@gmail.com>
2023-09-12 15:05:37 -0300

    configuracion deploy vercel

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
+const port = process.env.PORT || 8080;
 const cors = require("cors");
 const mercadopago = require("mercadopago");
 
 app.use(express.json());
 app.use(cors());
+
 
 mercadopago.configure({
     access_token: "TEST-1291734623694321-081112-c71ea35dd9e35c6dec44f1b84ca0b144-91553276",
@@ -14,7 +16,7 @@ app.get('/', function (req, res) {
     res.send('el servidor de mercadopago funciona');
 })
 
- 
+
 
 app.post("/create_preference", async (req, res) => {
     try {
@@ -27,8 +29,8 @@ app.post("/create_preference", async (req, res) => {
                 },
             ],
             back_urls: {
-                success: "http://localhost:3000",
-                failure: "http://localhost:3000",
+                success: "http://localhost:8080",
+                failure: "http://localhost:8080",
                 pending: "",
             },
             auto_return: "approved",
@@ -47,8 +49,8 @@ app.post("/create_preference", async (req, res) => {
 
 
 
-app.listen(8080, () => {
-    console.log('server is running on port 8080');
+app.listen(port, () => {
+    console.log(`server is running on port http://localhost:${port}`);
 });
 
 
