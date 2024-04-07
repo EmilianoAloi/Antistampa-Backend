76be0c357a1da9aed3d73457341e9850b6f0fcf6
Emiliano Aloi <emilianoaloi.dev@gmail.com>
2024-04-07 17:53:12 -0300

    acces token de prueba - MP Antistampa

--- index.js
+++ index.js
@@ -9,14 +9,12 @@ app.use(cors());
 
 
 mercadopago.configure({
-    access_token: "TEST-1291734623694321-081112-c71ea35dd9e35c6dec44f1b84ca0b144-91553276",
+    access_token: "TEST-7899621053516003-031112-f1e27f6ce8eae7bff80e5b0a139298f4-170171815",
 });
 
 app.get('/', function (req, res) {
     res.send('el servidor de mercadopago funciona');
 })
-
-
 
 app.post("/create_preference", async (req, res) => {
     try {
@@ -58,100 +56,3 @@ app.listen(port, () => {
 
 
 
-
-///////////////////////
-
-
-
-// app.post("/create_preference", async (req, res) => {
-//     try {
-//         const items = req.body.items; // Obtiene el array de productos del cuerpo de la solicitud
-
-//         // Crea un array de objetos de items para la preferencia de MercadoPago
-//         const preferenceItems = items.map(item => ({
-//             title: item.title,
-//             unit_price: Number(item.unit_price),
-//             quantity: Number(item.quantity),
-//             description: item.description,
-//             picture_url: item.picture_url, // Agrega la URL de la imagen del producto
-//             color: item.color, // Agrega el color del producto
-//             size: item.size, // Agrega el tamaño del producto
-//         }));
-
-//         // Construye la preferencia con los items actualizados
-//         let preference = {
-//             items: preferenceItems,
-//             back_urls: {
-//                 success: "http://localhost:3000",
-//                 failure: "http://localhost:3000",
-//                 pending: "",
-//             },
-//             auto_return: "approved",
-//         };
-
-//         const response = await mercadopago.preferences.create(preference);
-
-//         res.json({
-//             id: response.body.id,
-//         });
-//     } catch (error) {
-//         console.log(error);
-//         res.status(500).json({ error: 'Error al crear la preferencia' });
-//     }
-// });
-
-
-//////////////////////
-/////////////////
-
-
-// app.post("/create_preference", async (req, res) => {
-//     try {
-
-        
-//         const items = req.body.items; // Obtiene el array de productos del cuerpo de la solicitud
-
-
-//         const preferenceItems = items.map(item => ({
-//             title: item.name, // Usar la propiedad del objeto en el array
-//             unit_price: Number(item.newTotal), // Usar la propiedad del objeto en el array
-//             quantity: Number(item.quantity),
-//             picture_url: item.img, // Usar la propiedad del objeto en el array
-//             color: item.color, // Usar la propiedad del objeto en el array
-//             size: item.size, // Usar la propiedad del objeto en el array
-//         }));
-
-
-//         let preference = {
-
-//             items: preferenceItems,
-
-//             back_urls: {
-//                 success: "http://localhost:3000",
-//                 failure: "http://localhost:3000",
-//                 pending: "",
-//             },
-//             auto_return: "approved",
-//         };
-
-//         //     mercadopago.preferences
-//         //         .create(preference)
-//         //         .then(function (response) {
-//         //             res.json({
-//         //                 id: response.body.id,
-//         //             });
-//         //         })
-//         //         .catch(function (error) {
-//         //             console.log(error);
-//         //         });
-
-//         const response = await mercadopago.preferences.create(preference);
-
-//         res.json({
-//             id: response.body.id,
-//         });
-//     } catch (error) {
-//         console.log(error);
-//         res.status(500).json({ error: 'Error al crear la preferencia' });
-//     }
-// });
