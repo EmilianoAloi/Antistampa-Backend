0068d1fbdda3b452605775a7f1a56a9115d151e4
Emiliano Aloi <emilianoaloi.dev@gmail.com>
2024-04-15 20:03:07 -0300

    actualizacion back urls

--- index.js
+++ index.js
@@ -27,8 +27,8 @@ app.post("/create_preference", async (req, res) => {
                 },
             ],
             back_urls: {
-                success: "http://localhost:3000/confirmed",
-                failure: "http://localhost:3000/",
+                success: "https://antistampa.netlify.app/confirmed",
+                failure: "https://antistampa.netlify.app/",
                 pending: "",
             },
             auto_return: "approved",
